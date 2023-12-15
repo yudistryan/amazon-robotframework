499ff74b7484ea0477138568b73692182855d6ca
Yudis Yufanria <yudis.yufanria@wallet-codes.com>
2023-12-15 12:48:35 +0700

    Add some data processing functions

--- Libraries/dataProcessing.py
+++ Libraries/dataProcessing.py
@@ -1,2 +1,16 @@
+import math
+
 def revertList(list):
     return list[::-1]
+
+def sumAllPrice(list):
+    total = 0
+    for price in list:
+        total += float(price.split('$')[1])
+    return str(total)
+
+def getTax(total_purchase):
+    return str('{:.2f}'.format(float(total_purchase)*0.08,2))
+
+def getCurrencyValue(string):
+    return string.split('$')[1]
